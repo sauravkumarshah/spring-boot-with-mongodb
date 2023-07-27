98170e4ee4e14b1b32935feb2083b27a27bfefa3
Saurav Kumar <132904508+sauravkumarshah@users.noreply.github.com>
2023-07-28 01:05:23 +0530

    Update SpringBootMongodbApplicationTests.java

--- src/test/java/com/springboot/mongodb/SpringBootMongodbApplicationTests.java
+++ src/test/java/com/springboot/mongodb/SpringBootMongodbApplicationTests.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -47,10 +49,6 @@ class SpringBootMongodbApplicationTests {
 
 	private static List<EmployeeDTO> employeeResponse = new ArrayList<>();
 
-	// @Container
-	// private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(
-	// 		DockerImageName.parse("mongo:4.0.10"));
-
 	@Container
 	private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.4.3"));
 
@@ -58,7 +56,6 @@ class SpringBootMongodbApplicationTests {
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
 		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
-
 
 	static {
 		mongoDBContainer.start();
